bfa507bba12d3c980cc5c456b56b2bca425579d9
Saasman <dferasmus@gmail.com>
2025-09-13 10:07:06 +0200

    saving for tools

--- dungeon-mobile/Globals/hex.gd
+++ dungeon-mobile/Globals/hex.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node
 
 # Axial coordinates (q, r) where q + r + s = 0
---
+++ dungeon-mobile/Scenes/models/stationary.gd
@@ -0,0 +1,8 @@
+@tool
+extends Node3D
+
+@export var group = "ground"
+@export var type = "stone"
+
+func _ready() -> void:
+	pass
